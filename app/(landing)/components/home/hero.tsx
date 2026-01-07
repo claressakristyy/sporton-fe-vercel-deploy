bd7c1ca9c53c916f6e2253fca2ebb3d1b22a1a63
nessa <nidiaclaress@gmail.com>
2026-01-07 11:25:30 +0700

    first commit

---
+++ app/(landing)/components/home/hero.tsx
@@ -0,0 +1,56 @@
+import { FiFastForward } from "react-icons/fi";
+import Button from "../ui/button";
+import Image from "next/image";
+
+const HeroSection = () => {
+    return (
+        <section id="hero-section" className="container mx-auto h-screen flex">
+            <div className="relative self-center">
+            <img 
+            src="/images/img-basketball.svg" 
+            width={432} 
+            height={423} 
+            alt="image sporton"
+            className="grayscale absolute left-20 -top-20"
+            />
+             <div className="relative ml-40 w-full">
+             <div className="text-orange-500 italic">Friday Sale. 50%</div>
+             <h1 className="font-extrabold text-[75px] italic bg-gradient-to-b leading-tight from-black to-[#979797] bg-clip-text text-transparent">
+                WEAR YOUR <br /> TOP QUALITY <br /> SPOTSWEAR</h1>
+                <p className="w-1/2 mt-10 leading-loose">
+                Engineered for endurance and designed for speed. 
+                Experience gear that moves as fast as you do. 
+                Premium fabrics. Unmatched comfort. Limitless motion.</p>
+                <div className="flex gap-5 mt-10">
+                    <Button>
+                        Explore More <FiFastForward/>
+                    </Button>
+                    <Button variant="ghost">
+                        Watch Video {""} 
+                        <Image
+                        src="/images/icon-play-video.svg"
+                        alt="icon playvideo" 
+                        width={29} 
+                        height={29}/>
+                    </Button>
+                </div>
+             </div>
+             <Image 
+             src="/images/img-hero.svg" 
+             width={700} 
+             height={950} 
+             alt="image sporton hero"
+             className="absolute -right-5 top-1/2 -translate-y-1/2"/>
+            </div>
+            <img 
+            src="/images/img-ornament-hero.svg" 
+            width={420} 
+            height={420} 
+            alt="image-sporton"
+            className="absolute -right-[200px] top-1/2 -translate-y-1/2"
+            />
+        </section>
+    );
+};
+
+export default HeroSection;
---
+++ app/(landing)/components/layout/header.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiSearch, FiShoppingBag } from "react-icons/fi";
+
+const Header = () => {
+    return ( 
+    <header>
+        <div className="flex justify-between gap-10 container mx-auto py-7">
+        <Image 
+        src="/images/logo.svg" 
+        alt="sporton logo" 
+        width={127} 
+        height={30}
+     />
+     <nav className="flex gap-44 font-medium">
+        <Link href="#" className="relative after:conten-[''] after:block after:bg-orange-500 after:rounded-full after:h-[3px] after:w-2/3 after:absolute after:left-1/2 after:-translate-x-1/2"
+        >Home</Link>
+        <Link href="#">Category</Link>
+        <Link href="#">Explore Products</Link>
+     </nav>
+     <div className="flex gap-10">
+        <FiSearch size={24}/>
+        <div className="relative">
+         <FiShoppingBag size={24}/>
+         <div className="bg-orange-500 rounded-full w-3.5 h-3.5 absolute -top-1 -right-1 text-[10px] text-white text-center">
+            3
+         </div>
+        </div>
+     </div>
+     </div>
+    </header>
+        );
+}
+
+export default Header;
