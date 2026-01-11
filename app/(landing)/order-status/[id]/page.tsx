070a93267ffdee5fc6a469c9472ad089a2903e49
nessa <nidiaclaress@gmail.com>
2026-01-11 13:11:05 +0700

    fix space

--- app/(landing)/order-status/[id]/page.tsx
+++ app/(landing)/order-status/[id]/page.tsx
@@ -5,11 +5,11 @@ import OrderConfirmed from "../../components/order-status/order-confirmed";
 import OrderSubmitted from "../../components/order-status/order-submitted";
 
 const OrderStatus = () => {
-    const [isConfirmed, setIsConfirmed] = useState(false);
+    const [isConfirmed, setIsConfirmed] = useState(true);
     return (
     <main className="bg-gray-100 min-h-[80vh]">
-        <div className="max-w-5xl mx-auto py-20">
-            <h1 className="text-5xl font-bold text-center mb-11">Order Status</h1>
+        <div className="max-w-5xl mx-auto py-10">
+            <h1 className="text-5xl font-bold text-center mb-5">Order Status</h1>
         </div>
         {isConfirmed ? <OrderConfirmed/> : <OrderSubmitted/>}
     </main>
